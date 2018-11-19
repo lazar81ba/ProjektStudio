5aac00a89dd0d6e6f8e8880c852e7c0ec53da5dd
Bartłomiej Łazarczyk <lazar81ba@gmail.com>
2018-11-19 22:04:23 +0100

    Implementing rest controller for employee

--- BotRest/src/main/java/com/studio/service/EmployeeService.java
+++ BotRest/src/main/java/com/studio/service/EmployeeService.java
@@ -1,7 +1,11 @@
 package com.studio.service;
 
 import com.studio.model.Consultation;
+import com.studio.model.Employee;
+
+import java.util.List;
 
 public interface EmployeeService {
-
+    public List<Employee> getEmployeesWithNameAndSurname(String name, String surname);
+    public Employee getEmployeeWithUniversityNick(String university_nick);
 }
--- BotRest/src/main/java/com/studio/service/implementation/EmployeeServiceImpl.java
+++ BotRest/src/main/java/com/studio/service/implementation/EmployeeServiceImpl.java
@@ -7,8 +7,21 @@ import com.studio.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
+    @Autowired
+    EmployeeDAO employeeDAO;
 
+    @Override
+    public List<Employee> getEmployeesWithNameAndSurname(String name, String surname) {
+        return employeeDAO.findAllByNameAndSurname(name, surname);
+    }
+
+    @Override
+    public Employee getEmployeeWithUniversityNick(String university_nick) {
+        return employeeDAO.findByUniversityNick(university_nick);
+    }
 }
